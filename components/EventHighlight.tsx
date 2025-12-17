0f969a85e7849a0c477b1f660f751fbf40007257
Marcia Kgaphola <integratedwellth@gmail.com>
2025-12-17 11:54:19 +0200

    Update EventHighlight.tsx

--- components/EventHighlight.tsx
+++ components/EventHighlight.tsx
@@ -75,7 +75,7 @@ const EventHighlight: React.FC = () => {
               </div>
               <div className="flex items-center text-gray-700">
                 <Clock className="w-5 h-5 mr-3 text-brand-500" />
-                <span>09:00 AM - 13:00 PM</span>
+                <span>09:00 AM - 16:30 PM</span>
               </div>
               <div className="flex items-center text-gray-700">
                 <MapPin className="w-5 h-5 mr-3 text-brand-500" />
