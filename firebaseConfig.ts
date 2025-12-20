5a2b0fb25763f268bdf401a5100375085774e9f7
Marcia Kgaphola <integratedwellth@gmail.com>
2025-12-20 15:05:13 +0200

    Update firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,16 +3,18 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"; 
 
+// Your NEW working US-based configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCqkjyN8etq3GcoJjdr9SyPewMCMtZRKdg",
-  authDomain: "integratedwellth-a288f.firebaseapp.com",
-  projectId: "integratedwellth-a288f",
-  storageBucket: "integratedwellth-a288f.firebasestorage.app",
-  messagingSenderId: "271338433506",
-  appId: "1:271338433506:web:2419f6f94af41fd691a7bb",
-  measurementId: "G-R6CYBXMR5C"
+  apiKey: "AIzaSyByrYwESDF82C9AiZxtg77eUfNcIVGOwmQ",
+  authDomain: "integratedwellthsolutions.firebaseapp.com",
+  projectId: "integratedwellthsolutions",
+  storageBucket: "integratedwellthsolutions.firebasestorage.app",
+  messagingSenderId: "841640688054",
+  appId: "1:841640688054:web:4c0ea463640ea0d26fa917",
+  measurementId: "G-KR4PRVGLLG"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
