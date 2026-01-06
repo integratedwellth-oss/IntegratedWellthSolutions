487bea8e05cae8231e923f6fb49de400f92d37a2
Marcia Kgaphola <integratedwellth@gmail.com>
2026-01-06 22:36:30 +0200

    Create ContactPage.tsx

---
+++ components/pages/ContactPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Contact from '../Contact';
+
+const ContactPage: React.FC = () => {
+  return (
+    <div className="pt-20 min-h-screen bg-gray-50">
+       <Contact />
+    </div>
+  );
+};
+
+export default ContactPage;
