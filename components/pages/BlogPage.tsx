18c9dbefc1f951a2b296fcf3fb09be1ee20907ce
Marcia Kgaphola <integratedwellth@gmail.com>
2026-01-06 22:35:58 +0200

    Create BlogPage.tsx

---
+++ components/pages/BlogPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BlogPost from '../BlogPost';
+
+const BlogPage: React.FC = () => {
+  return (
+    <div className="pt-10">
+      <BlogPost />
+    </div>
+  );
+};
+
+export default BlogPage;
