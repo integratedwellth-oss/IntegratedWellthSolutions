8a3f6ee26c2cb6fc50e28ef17b8bf4c2000f95ee
Marcia Kgaphola <integratedwellth@gmail.com>
2026-01-07 18:55:44 +0200

    Update Home.tsx

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import Hero from '../Hero';
 import TrustedBy from '../TrustedBy';
-import WhoWeHelp from '../WhoWeHelp';
 import Philosophy from '../Philosophy';
 import EventHighlight from '../EventHighlight';
 import Services from '../Services';
 import Audience from '../Audience';
+import FinancialHealthScore from '../FinancialHealthScore';
 import Testimonials from '../Testimonials';
 import Gallery from '../Gallery';
 import Contact from '../Contact';
 
-interface HomeProps {
-  onOpenAssessment: () => void;
-}
-
-const Home: React.FC<HomeProps> = ({ onOpenAssessment }) => {
+const Home: React.FC = () => {
   return (
     <div className="animate-fadeIn">
-      <Hero />
+      <div id="home"><Hero /></div>
       <TrustedBy />
-      <WhoWeHelp />
-      <Philosophy />
-      <div id="services-anchor"><Services /></div>
-      <Audience />
+      <div id="philosophy"><Philosophy /></div>
+      <div id="workshops"><EventHighlight /></div>
+      <div id="services"><Services /></div>
+      <div id="who-we-help"><Audience /></div>
+      <div id="assessment" className="py-20 bg-gray-50">
+        <div className="max-w-4xl mx-auto px-4 text-center mb-12">
+          <h2 className="text-3xl md:text-5xl font-black text-brand-900 uppercase">Financial Health Diagnostic</h2>
+        </div>
+        <FinancialHealthScore isOpen={true} onClose={() => {}} isModal={false} />
+      </div>
       <Testimonials />
       <Gallery />
-      <Contact />
+      <div id="contact"><Contact /></div>
     </div>
   );
 };
