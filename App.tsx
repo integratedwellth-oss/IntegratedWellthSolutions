d1028b9b7ec1083ed2f17507d41f2e91ffd88e6e
Marcia Kgaphola <integratedwellth@gmail.com>
2026-01-07 03:45:31 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 
-// Layout & Global Components
+// Global Layout Components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import WhatsAppButton from './components/WhatsAppButton';
@@ -9,9 +9,9 @@ import CookieConsent from './components/CookieConsent';
 import FloatingCTA from './components/FloatingCTA';
 import EventPopup from './components/EventPopup'; 
 
-// Pages
+// Page Hubs (Referencing src/components/pages/ per your file tree)
 import Home from './components/pages/Home';
-import Team from './Team';
+import TeamPage from './components/pages/TeamPage';
 import ServicesPage from './components/pages/ServicesPage';
 import ContactPage from './components/pages/ContactPage';
 import WorkshopPage from './components/pages/WorkshopPage';
@@ -19,20 +19,27 @@ import WhoWeHelpPage from './components/pages/WhoWeHelpPage';
 import BlogPage from './components/pages/BlogPage';
 import AdminDashboard from './components/pages/AdminDashboard';
 import AssessmentPage from './components/pages/AssessmentPage';
-import AssessmentSuccess from './components/pages/AssessmentSuccess'; // Import Success Page
+import AssessmentSuccess from './components/pages/AssessmentSuccess';
 
-// Solution Routes
+// Legal
+import PrivacyPolicy from './components/PrivacyPolicy';
+
+// Solution Details (Referencing src/components/audiences/)
 import StartupSolutions from './components/audiences/StartupSolutions';
 import BusinessSolutions from './components/audiences/BusinessSolutions';
+import NPOSolutions from './components/audiences/NPOSolutions';
+import IndividualSolutions from './components/audiences/IndividualSolutions';
 
 const App: React.FC = () => {
   const { pathname } = useLocation();
   const [showEvent, setShowEvent] = useState(false);
 
+  // Reset scroll on every navigation
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
+  // Global Event Protocol: Popup triggers after 5 seconds
   useEffect(() => {
     const timer = setTimeout(() => {
       if (!sessionStorage.getItem('hasSeenEvent')) {
@@ -46,24 +53,43 @@ const App: React.FC = () => {
   return (
     <div className="font-sans text-gray-900 bg-white min-h-screen flex flex-col">
       <Navbar />
+      
       <main className="flex-grow">
         <Routes>
+          {/* Base Navigation */}
           <Route path="/" element={<Home />} />
-          <Route path="/team" element={<Team />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/team" element={<TeamPage />} />
           <Route path="/services" element={<ServicesPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/workshops" element={<WorkshopPage />} />
-          <Route path="/assessment" element={<AssessmentPage />} />
-          <Route path="/assessment-success" element={<AssessmentSuccess />} /> {/* New Route */}
+          <Route path="/who-we-help" element={<WhoWeHelpPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+          
+          {/* Functional Destinations */}
           <Route path="/admin" element={<AdminDashboard />} />
-          {/* Add other solution routes here */}
+          <Route path="/assessment" element={<AssessmentPage />} />
+          <Route path="/assessment-success" element={<AssessmentSuccess />} />
+
+          {/* Deep-Dive Audience Routes */}
+          <Route path="/startups" element={<StartupSolutions />} />
+          <Route path="/existing-business" element={<BusinessSolutions />} />
+          <Route path="/npos" element={<NPOSolutions />} />
+          <Route path="/individuals" element={<IndividualSolutions />} />
         </Routes>
       </main>
+
       <Footer />
       <WhatsAppButton />
       <CookieConsent />
       <FloatingCTA />
-      <EventPopup isOpen={showEvent} onClose={() => setShowEvent(false)} />
+      
+      {/* Managed Modals */}
+      <EventPopup 
+        isOpen={showEvent} 
+        onClose={() => setShowEvent(false)} 
+      />
     </div>
   );
 };
