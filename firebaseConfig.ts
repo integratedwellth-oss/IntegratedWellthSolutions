3f95f9f0259fa5b3a640d1fb278c69139c36d89c
Marcia Kgaphola <integratedwellth@gmail.com>
2026-01-07 19:00:00 +0200

    Update firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,9 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider } from "firebase/auth"; 
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// Your NEW working US-based configuration
 const firebaseConfig = {
   apiKey: "AIzaSyByrYwESDF82C9AiZxtg77eUfNcIVGOwmQ",
   authDomain: "integratedwellthsolutions.firebaseapp.com",
@@ -14,11 +12,9 @@ const firebaseConfig = {
   measurementId: "G-KR4PRVGLLG"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
 
-export { app, analytics, db, auth, googleProvider };
+export default app;
