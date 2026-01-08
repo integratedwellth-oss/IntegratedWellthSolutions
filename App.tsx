53394c24c5c5018955fc90c974521ce47d2c4a71
Marcia Kgaphola <integratedwellth@gmail.com>
2026-01-08 17:00:15 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,50 +1,30 @@
-import React, { useEffect } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-
-// Global Layout
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-
-// Pages - Consolidated Imports
 import Home from './components/pages/Home';
 import TeamPage from './components/pages/TeamPage';
-import ServicesPage from './components/pages/ServicesPage';
-import ContactPage from './components/pages/ContactPage';
-import WorkshopPage from './components/pages/WorkshopPage';
-import WhoWeHelpPage from './components/pages/WhoWeHelpPage';
-import BlogPage from './components/pages/BlogPage';
-import AssessmentPage from './components/pages/AssessmentPage';
 import AssessmentSuccess from './components/pages/AssessmentSuccess';
 import AdminDashboard from './components/pages/AdminDashboard';
 
-const App: React.FC = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
+function App() {
   return (
-    <div className="min-h-screen flex flex-col font-sans">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-grow pt-20">
+      <main className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/team" element={<TeamPage />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/workshops" element={<WorkshopPage />} />
-          <Route path="/who-we-help" element={<WhoWeHelpPage />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/assessment" element={<AssessmentPage />} />
           <Route path="/assessment-success" element={<AssessmentSuccess />} />
           <Route path="/admin" element={<AdminDashboard />} />
+          {/* Fallback to Home if path is wrong */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
       <Footer />
     </div>
   );
-};
+}
 
 export default App;
