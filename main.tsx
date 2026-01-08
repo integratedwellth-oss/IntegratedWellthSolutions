24bc18af2aa374bf0eae038db49889cbacb05525
Marcia Kgaphola <integratedwellth@gmail.com>
2026-01-08 22:10:15 +0200

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -4,17 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-// Safety check to prevent crashing if HTML loads partially
 const rootElement = document.getElementById('root');
 
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  );
-} else {
-  console.error("CRITICAL: Root element not found.");
+if (!rootElement) {
+  throw new Error('Root element not found');
 }
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
