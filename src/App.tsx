48a11f879fae4b2726813e35370ae530b92bfd8c
Marcia Kgaphola <integratedwellth@gmail.com>
2026-01-09 15:11:41 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import WarRoom from './components/WarRoom';
-import Services from './pages/Services'; // NEW IMPORT
-import NeuralCFO from './components/NeuralCFO'; // NEW IMPORT
+import Services from './pages/Services'; 
+import Team from './pages/Team'; // NEW
+import WhoWeHelp from './pages/WhoWeHelp'; // NEW
+import Contact from './pages/Contact'; // NEW
+import NeuralCFO from './components/NeuralCFO';
 
 const App = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       
-      {/* THE INTELLIGENCE LAYER - Active on all pages */}
+      {/* THE INTELLIGENCE LAYER */}
       <NeuralCFO /> 
       
       <main className="flex-grow">
@@ -19,6 +22,9 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/war-room" element={<WarRoom />} />
           <Route path="/services" element={<Services />} />
+          <Route path="/team" element={<Team />} /> {/* NEW */}
+          <Route path="/who-we-help" element={<WhoWeHelp />} /> {/* NEW */}
+          <Route path="/contact" element={<Contact />} /> {/* NEW */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
