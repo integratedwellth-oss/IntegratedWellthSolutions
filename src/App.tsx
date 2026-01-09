675d11f7d817eb192e05f2dd4be332a44ebe1769
Marcia Kgaphola <integratedwellth@gmail.com>
2026-01-09 20:58:44 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer'; // NEW IMPORT
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import WarRoom from './components/WarRoom';
 import Services from './pages/Services'; 
 import Team from './pages/Team'; 
 import WhoWeHelp from './pages/WhoWeHelp'; 
 import Contact from './pages/Contact'; 
+import Gallery from './pages/Gallery'; // NEW IMPORT
 import NeuralCFO from './components/NeuralCFO';
 import Admin from './pages/Admin'; 
 
@@ -15,8 +16,6 @@ const App = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      
-      {/* THE INTELLIGENCE LAYER */}
       <NeuralCFO /> 
       
       <main className="flex-grow">
@@ -26,13 +25,13 @@ const App = () => {
           <Route path="/services" element={<Services />} />
           <Route path="/team" element={<Team />} /> 
           <Route path="/who-we-help" element={<WhoWeHelp />} /> 
+          <Route path="/gallery" element={<Gallery />} /> {/* NEW ROUTE */}
           <Route path="/contact" element={<Contact />} />
           <Route path="/command" element={<Admin />} /> 
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
       
-      {/* NEW PROFESSIONAL FOOTER */}
       <Footer />
     </div>
   );
