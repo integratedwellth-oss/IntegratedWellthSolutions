6e60acf8cda007fb887370504bf6148b77acb2c7
Marcia Kgaphola <integratedwellth@gmail.com>
2026-01-13 13:11:49 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import { SYSTEM_STATUS } from './constants';
+import Home from './pages/Home';
+import WarRoom from './pages/WarRoom';
+import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
-    <Layout>
-      <div className="flex flex-col items-center justify-center py-20 text-center">
-        <h1 className="text-[#C5A059] text-5xl font-black uppercase tracking-[0.3em] mb-4">
-          Intelligence Online
-        </h1>
-        <p className="text-gray-500 font-mono text-sm uppercase">{SYSTEM_STATUS}</p>
-      </div>
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/war-room" element={<WarRoom />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
