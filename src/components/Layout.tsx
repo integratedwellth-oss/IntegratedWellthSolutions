0bc2b7dd8f67093e2464d317cdaac86f4a2a8e7e
Marcia Kgaphola <integratedwellth@gmail.com>
2026-01-13 19:56:05 +0200

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import WhatsAppButton from './WhatsAppButton';
-import { HelmetProvider } from 'react-helmet-async';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <HelmetProvider>
-      {/* This 'bg-[#05070a]' is the direct fix for the white screen issue */}
-      <div className="min-h-screen flex flex-col bg-[#05070a] text-white">
-        <Navbar />
-        <main className="flex-grow">
-          {children}
-        </main>
-        <Footer />
-        <WhatsAppButton />
-      </div>
-    </HelmetProvider>
+    <div className="min-h-screen flex flex-col bg-[#f8f9fa]">
+      <Navbar />
+      <main className="flex-grow pt-32">
+        {children}
+      </main>
+      <Footer />
+    </div>
   );
 }
