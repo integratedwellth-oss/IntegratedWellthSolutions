28eac239d2c561c8648be264d094fd487cad0ee7
Marcia Kgaphola <integratedwellth@gmail.com>
2026-01-13 19:01:58 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import WarRoom from './pages/WarRoom';
+import Team from './pages/Team';
 import Admin from './pages/Admin';
-import Privacy from './pages/Privacy';
-import Terms from './pages/Terms';
 import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
     <Router>
-      <Suspense fallback={<div className="bg-[#05070a] min-h-screen"/>}>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/war-room" element={<WarRoom />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/privacy" element={<Privacy />} />
-            <Route path="/terms" element={<Terms />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </Suspense>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/war-room" element={<WarRoom />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
