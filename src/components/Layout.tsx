543b95390e088b41705d72eb0e3ec3ea2f340f28
Marcia Kgaphola <integratedwellth@gmail.com>
2026-01-13 17:45:29 +0200

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,8 @@ import { HelmetProvider } from 'react-helmet-async';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <HelmetProvider>
-      <div className="min-h-screen flex flex-col bg-[#05070a]">
+      {/* This 'bg-[#05070a]' is the direct fix for the white screen issue */}
+      <div className="min-h-screen flex flex-col bg-[#05070a] text-white">
         <Navbar />
         <main className="flex-grow">
           {children}
