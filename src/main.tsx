866f03bea1abf18cf2121828e2d347ea5b4ff135
Marcia Kgaphola <integratedwellth@gmail.com>
2026-01-14 10:34:27 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css' // THIS IS WHAT ACTIVATES THE BEAUTY
+import './index.css' // THIS MUST BE HERE
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
